71529e40b7eaf6961e495dcd600ed303d14ce7c1
Michał Piasecki <michal@elexpress.pl>
2021-08-17 16:12:58 +0200

    Added behavior change on config

--- PowerDisplay/HarmonyExtraInfoText.cs
+++ PowerDisplay/HarmonyExtraInfoText.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using TaleWorlds.CampaignSystem;
 
 namespace PowerDisplay {
-	[HarmonyPatch(typeof(PartyNameplateVM), "Wounded", MethodType.Getter)]
-	public class HarmonyWounded {
+	[HarmonyPatch(typeof(PartyNameplateVM), "ExtraInfoText", MethodType.Getter)]
+	public class HarmonyExtraInfoText {
 		public static void Postfix(PartyNameplateVM __instance, ref string __result) {
-			if (!string.IsNullOrEmpty(__result) && !__result.Contains('[')) {
+			if (__result is not null && !__result.Contains('[')) {
 				MobileParty party = __instance.Party;
-				string modified = __result + $" [{(int)party.GetTotalStrengthWithFollowers()}]";
+				string modified = $" [{(int)party.GetTotalStrengthWithFollowers()}] " + __result;
 				__result = modified;
 			}
 		}
--- PowerDisplay/SubModule.cs
+++ PowerDisplay/SubModule.cs
@@ -1,13 +1,44 @@
 ﻿using HarmonyLib;
+using SandBox.ViewModelCollection.Nameplate;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
 
 namespace PowerDisplay {
 	public class SubModule : MBSubModuleBase {
 
+		private readonly string ConfigFile = BasePath.Name + "Modules/PowerDisplay/Config.xml";
+
 		protected override void OnSubModuleLoad() {
 			var harmony = new Harmony("PowerDisplay");
+			Config config = ReadXML(ConfigFile);
 
-			harmony.PatchAll();
+			if (config.DisplayOnInspect) {
+				var original = typeof(PartyNameplateVM).GetProperty("ExtraInfoText").GetGetMethod();
+				var postfix = typeof(HarmonyExtraInfoText).GetMethod("Postfix");
+
+				harmony.Patch(original, null, new HarmonyMethod(postfix));
+			} else {
+				var original = typeof(PartyNameplateVM).GetProperty("Count").GetGetMethod();
+				var postfix = typeof(HarmonyCount).GetMethod("Postfix");
+
+				harmony.Patch(original, null, new HarmonyMethod(postfix));
+			}
+		}
+
+		private Config ReadXML(string xml) {
+			Config config = new();
+			XmlSerializer serializer = new XmlSerializer(typeof(Config));
+			//string file = File.ReadAllText(xml, Encoding.UTF8);
+
+			using (FileStream fs = new FileStream(xml, FileMode.Open)) {
+				config = (Config)serializer.Deserialize(fs);
+			}
+
+			return config;
 		}
 	}
 }
