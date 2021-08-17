0c611708c8b6dbd9bd0fd2a9b3d728f27834d93d
Michał Piasecki <michal@elexpress.pl>
2021-08-17 12:28:52 +0200

    Created wounded count patch

---
+++ PowerDisplay/HarmonyWounded.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using SandBox.ViewModelCollection.Nameplate;
+using System.Linq;
+using TaleWorlds.CampaignSystem;
+
+namespace PowerDisplay {
+	[HarmonyPatch(typeof(PartyNameplateVM), "Wounded", MethodType.Getter)]
+	public class HarmonyWounded {
+		public static void Postfix(PartyNameplateVM __instance, ref string __result) {
+			if (!string.IsNullOrEmpty(__result) && !__result.Contains('[')) {
+				MobileParty party = __instance.Party;
+				string modified = __result + $" [{(int)party.GetTotalStrengthWithFollowers()}]";
+				__result = modified;
+			}
+		}
+	}
+}
